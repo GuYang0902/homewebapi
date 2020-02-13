56b94484462942ec85e6ce8ddec333b62c147c8a
yang <13482198105@163.com>
2020-02-13 23:03:58 +0800

    家里面的数据库

--- home.dbhelper/DBConfig.cs
+++ home.dbhelper/DBConfig.cs
@@ -4,6 +4,7 @@ namespace home.dbhelper
 {
     public static class DBConfig
     {
+        //mysql home.DB
         public static string HomeMysqlConn = "Server=192.168.9.7;Port=3306;Database=home.db;Uid=root;Pwd=a@1234567;default command timeout=20;Connection Timeout=5;";
     }
 }
